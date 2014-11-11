517b58170b4e00bf04ebff918fae33dbb012a46f
David Banks <amoebae@gmail.com>
2014-11-11 11:30:46 +0000

    changes before git import

--- sync_music.sh
+++ sync_music.sh
@@ -3,7 +3,9 @@
 # sync_music - sync music
 # first create the subset forest, then sync with --delete
 
+mountpoint=/mnt/tmp/MUSIC
+
 forest=$(mktemp -dt forest-XXXXXXXX)
-mzscheme ~/ripsys/forest.scm ~/music "$forest" || exit 1
-rsync -rtvL --delete --modify-window=1 "${forest}/" "/media/Sansa e280/music" \
+mzscheme ~/dev/old-ripsys/forest.scm ~/music "$forest" || exit 1
+sudo rsync -rtvL --delete --modify-window=1 "${forest}/" "${mountpoint}/" \
   && rm -r "$forest"
