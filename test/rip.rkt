1028974f78c45c7ec46bf45911e0d21c8a222766
David Banks <amoebae@gmail.com>
2015-01-18 19:07:31 +0000

    add test skeletons

---
+++ test/rip.rkt
@@ -0,0 +1,31 @@
+#lang racket/base
+
+(require rackunit "../rip.rkt")
+
+(test-case
+ "Output directory was created"
+ (fail))
+
+
+(test-case
+ "Track count matches TOC"
+ (fail))
+
+(test-case
+ "Directory name uses unique value"
+ (fail))
+
+
+
+(test-case
+ "Tracks are named sequentially"
+ (fail))
+
+
+(test-case
+ "Tracks are in valid WAV format"
+ (fail))
+
+(test-case
+ "Track times match CD TOC"
+ (fail))
