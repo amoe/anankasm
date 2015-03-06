de481e994e10c25433331d047b139196bd80050b
David Banks <david.banks@icrossing.co.uk>
2015-03-06 10:52:54 +0000

    clean up intermediate rip files

--- entry-point.rkt
+++ entry-point.rkt
@@ -6,7 +6,8 @@
 (match (current-command-line-arguments)
   ((vector output)
    (rip "FOOBAR")
-   (encode "/home/amoe/.anankasm/rip/FOOBAR" output))
+   (encode "/home/amoe/.anankasm/rip/FOOBAR" output)
+   (delete-directory/files "/home/amoe/.anankasm/rip/FOOBAR"))
   (else
    (error 'something "die")))
    
