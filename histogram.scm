51dd33b86a9ff6b98d72b59c8978084563115de7
David Banks <david.banks@icrossing.co.uk>
2015-03-09 17:15:26 +0000

    fix histogram for racket

--- histogram.scm
+++ histogram.scm
@@ -1,18 +1,21 @@
-(module histogram scheme
-  (provide histogram frequency>?)
+#lang racket
 
-  
+
+(provide histogram frequency>?)
+
+
 ; This is roughly a histogram, anyway - call it with a sorted list.
 ; It returns an alist of (item . frequency)
-  (define (histogram lst)
-    (define (iter n item lst)
-      (cond
-       ((null? lst)  (cons (cons item n) '()))
-       ((equal? (car lst) item)
-        (iter (+ n 1) item (cdr lst)))
-       (else (cons (cons item n)
-                   (iter 1 (car lst) (cdr lst))))))
-    
-    (iter 0 (car lst) lst))
+(define (histogram lst)
+  (define (iter n item lst)
+    (cond
+     ((null? lst)  (cons (cons item n) '()))
+     ((equal? (car lst) item)
+      (iter (+ n 1) item (cdr lst)))
+     (else (cons (cons item n)
+		 (iter 1 (car lst) (cdr lst))))))
+  
+  (iter 0 (car lst) lst))
 
-  (define (frequency>? x y) (> (cdr x) (cdr y))))
+(define (frequency>? x y) (> (cdr x) (cdr y)))
+
