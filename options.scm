5de99d266beb242a49e601de986adfb0939ea982
David Banks <david.banks@icrossing.co.uk>
2015-03-09 17:14:20 +0000

    translate options.scm to racket syntax

--- options.scm
+++ options.scm
@@ -1,100 +1,101 @@
 ; This module holds options and constants that reconfigure themselves
 ; based on said options
 
-(module options scheme
-  (require scheme/cmdline)
-  (require srfi/1)
-  (require srfi/78)
+#lang racket
 
-  (provide
-    global-tag-list
-    local-tag-list
-    filename-template
-    configure
-    option
-    option!)
+(require racket/cmdline)
+(require srfi/1)
+(require srfi/78)
 
-  (define *options*
-    '((va-mode             . #f)
-     (mastering-disparity  . #f)
-     (preserve-mtimes      . #t)
-     (debug                . #t)
-     (verbose              . #f)
-     (number-automatically . #f)
-     (skip-replaygain . #f)))
+(provide
+ global-tag-list
+ local-tag-list
+ filename-template
+ configure
+ option
+ option!)
 
-  (define (configure args)
-    (command-line
-     #:program "naturalize"
-     #:argv args
-     #:once-each
-     (("-V" "--va-mode")             "Various Artists mode"
-                                      (option! 'va-mode #t))
-     (("-v" "--verbose")             "show output from subprocesses"
-                                      (option! 'verbose #t))
-     (("-M" "--mastering-disparity") "do not scan album gain"
-                                      (option! 'mastering-disparity #t))
-     (("-P" "--mangle-mtimes")       "don't preserve file mtimes" #t)
-     (("-n" "--number-automatically") "automatically add track numbers"
-                                      (option! 'number-automatically #t))
-     (("-R" "--skip-replaygain") "do not apply replaygain"
-                                 (option! 'skip-replaygain #t))
-     #:args (path1 . paths)
-     (cons path1 paths)))
+(define *options*
+  '((va-mode             . #f)
+    (mastering-disparity  . #f)
+    (preserve-mtimes      . #t)
+    (debug                . #t)
+    (verbose              . #f)
+    (number-automatically . #f)
+    (skip-replaygain . #f)))
 
-  (define (global-tag-list)
-    (if (option 'va-mode)
-        '(album date genre)
-        '(artist album date genre)))
+(define (configure args)
+  (command-line
+   #:program "naturalize"
+   #:argv args
+   #:once-each
+   (("-V" "--va-mode")             "Various Artists mode"
+    (option! 'va-mode #t))
+   (("-v" "--verbose")             "show output from subprocesses"
+    (option! 'verbose #t))
+   (("-M" "--mastering-disparity") "do not scan album gain"
+    (option! 'mastering-disparity #t))
+   (("-P" "--mangle-mtimes")       "don't preserve file mtimes" #t)
+   (("-n" "--number-automatically") "automatically add track numbers"
+    (option! 'number-automatically #t))
+   (("-R" "--skip-replaygain") "do not apply replaygain"
+    (option! 'skip-replaygain #t))
+   #:args (path1 . paths)
+   (cons path1 paths)))
 
-  (define (local-tag-list)
-    (if (option 'va-mode)
-        '(tracknumber artist title)
-        '(tracknumber title)))
+(define (global-tag-list)
+  (if (option 'va-mode)
+      '(album date genre)
+      '(artist album date genre)))
 
-  (define (filename-template)
-    (if (option 'va-mode)
-        "/home/amoe/music/various/%d-%A/%T-%a_-_%t"
-        "/home/amoe/music/%a/%d-%A/%T-%t"))
+(define (local-tag-list)
+  (if (option 'va-mode)
+      '(tracknumber artist title)
+      '(tracknumber title)))
 
-  (define (option name)
-    (let ((opt (assq name *options*)))
-      (if opt
-          (cdr opt)
-          (error "unknown option"))))
+(define (filename-template)
+  (if (option 'va-mode)
+      "/home/amoe/music/various/%d-%A/%T-%a_-_%t"
+      "/home/amoe/music/%a/%d-%A/%T-%t"))
 
-  (define (option! name val)
-    (set! *options*
-          (map
-           (lambda (pair)
-             (let ((key (car pair)))
-               (if (eq? key name)
-                   (cons key val)
-                   pair)))
-           *options*)))
+(define (option name)
+  (let ((opt (assq name *options*)))
+    (if opt
+	(cdr opt)
+	(error "unknown option"))))
 
-  (define (alist? x)
-    (every pair? x))
+(define (option! name val)
+  (set! *options*
+	(map
+	 (lambda (pair)
+	   (let ((key (car pair)))
+	     (if (eq? key name)
+		 (cons key val)
+		 pair)))
+	 *options*)))
 
-  (define (test)
-    (check (alist? *options*) => #t)
+(define (alist? x)
+  (every pair? x))
 
-    (let ((filenames '("test1.mp3" "test2.mp3")))
-      (check (configure filenames) => filenames))
+(define (test)
+  (check (alist? *options*) => #t)
 
-    (check (list? (global-tag-list)) => #t)
-    (check (list? (local-tag-list)) => #t)
+  (let ((filenames '("test1.mp3" "test2.mp3")))
+    (check (configure filenames) => filenames))
 
-    (check (string? (filename-template)) => #t)
+  (check (list? (global-tag-list)) => #t)
+  (check (list? (local-tag-list)) => #t)
 
-    (let ((value (option 'va-mode)))
-      (check (boolean? value) => #t)
-      (check (void? (option! 'va-mode #t)) => #t)
-      (check (option 'va-mode) => #t)
-      (option! 'va-mode value))))
+  (check (string? (filename-template)) => #t)
 
-    
+  (let ((value (option 'va-mode)))
+    (check (boolean? value) => #t)
+    (check (void? (option! 'va-mode #t)) => #t)
+    (check (option 'va-mode) => #t)
+    (option! 'va-mode value)))
 
 
-    
+
+
+
 
